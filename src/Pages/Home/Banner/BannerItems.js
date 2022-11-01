d913486f679dc9172c2c096ed9b1d86d67773cd9
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-01 21:54:38 +0600

    add home page about and login and register page and authentication

---
+++ src/Pages/Home/Banner/BannerItems.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const BannerItems = ({slide}) => {
+  const {image,id,prev,next} = slide;
+  return (
+    <div id={`slide${id}`} className="carousel-item relative w-full">
+    <div className='img-gradient'>
+    <img src={image} className="w-full rounded-xl" />
+    </div>
+    <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+     <h1 className='text-6xl font-bold text-white'>
+      Affordable <br />
+      Price for Car <br />
+      Servicing
+     </h1>
+    </div>
+    <div className="absolute flex justify-end transform -translate-y-1/2 left-24 w-2/5 top-1/2">
+   <p className='text-white text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque nulla rem veritatis. Obcaecati, nobis sint neque architecto dignissimos error ipsa.</p>
+    </div>
+
+
+    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 w-2/5 top-3/4">
+    <button className="btn btn-warning mr-5">Warning</button>
+    <button className="btn btn-outline btn-warning">Warning</button>
+    </div>
+
+
+
+
+
+
+    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a> 
+      <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  );
+};
+
+export default BannerItems;
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,72 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import login from '../../assets/images/login/login.svg'
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+
+const Login = () => {
+  const{login}=useContext(AuthContext);
+  const handleLogin = event =>{
+    event.preventDefault();
+    const form =event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    login(email,password)
+    .then(result=>{
+      const user = result.user;
+      console.log(user);
+    })
+    .then(err=>console.error(err))
+
+  }
+  return (
+    <div className="hero my-20">
+      <div className="hero-content md:grid-cols-2 gap-20 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+         
+          <img className="w-3/4" src={login} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100  py-20">
+          <form onSubmit={handleLogin} className="card-body">
+          <h1 className="text-5xl text-center font-bold">Login now!</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                name="email"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                name="password"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary " type="submit" value="Login" />
+             
+            </div>
+          </form>
+          <p className="text-center">Don't have An Account? <Link className="text-orange-600 font-bold" to='/signup'>Sign Up</Link></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
